ca97a001e86a34e8568be3bf2a87f12d8139be52
PRaymium <Jokersli@mail.ru>
2023-11-17 18:43:13 +0300

    implemented  AppHeader adaptive added global-container max-width

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,48 +1,167 @@
 <template>
   <header class="header">
     <div class="container">
-      <div class="header__container">
-        <div class="header-logo">
-          <a href="#" class="header-logo__link">Avion</a>
-        </div>
-        <div class="header-right">
-          <button class="header-btn header-search-toggle">
+      <div class="header-row header-row--top">
+        <div class="header-left header-btn-list">
+          <button class="header-btn header-btn--search">
             <IconSearch />
           </button>
-          <button class="header-btn header-menu-toggle">
+          <button class="header-btn header-btn--toggle">
             <IconBurgerMenu />
           </button>
         </div>
+        <div class="header-logo">
+          <router-link to="/" class="header-logo__link">Avion</router-link>
+        </div>
+        <div class="header-right header-btn-list">
+          <button class="header-btn header-btn--cart">
+            <IconShopcart />
+          </button>
+          <button class="header-btn header-btn--user">
+            <IconUser />
+          </button>
+        </div>
+      </div>
+      <div class="header-row header-row--bottom">
+        <nav class="header-nav">
+          <ul class="header-nav__list">
+            <li
+              class="header-nav__item"
+              v-for="(item, idx) of navItems"
+              :key="idx"
+            >
+              <router-link class="header-nav__link" :to="item.link">{{
+                item.title
+              }}</router-link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
 </template>
 
 <script setup>
-import IconSearch from '@/components/icons/IconSearch.vue'
-import IconBurgerMenu from '@/components/icons/IconBurgerMenu.vue'
+import IconSearch from './icons/IconSearch.vue'
+import IconBurgerMenu from './icons/IconBurgerMenu.vue'
+import IconUser from './icons/IconUser.vue'
+import IconShopcart from './icons/IconShopcart.vue'
+
+const navItems = [
+  {
+    title: 'Plant pots',
+    link: '/'
+  },
+  {
+    title: 'Ceramics',
+    link: '/'
+  },
+  {
+    title: 'Tables',
+    link: '/'
+  },
+  {
+    title: 'Chairs',
+    link: '/'
+  },
+  {
+    title: 'Crockery',
+    link: '/'
+  },
+  {
+    title: 'Tableware',
+    link: '/'
+  },
+  {
+    title: 'Cutlery',
+    link: '/'
+  }
+]
 </script>
 
 <style lang="scss">
 .header {
   &__container {
-    padding: 20px 0px;
-    display: flex;
     justify-content: space-between;
   }
 
-  &-logo__link {
-    font-family: $font-clash-display;
-    font-size: 24px;
+  &-row {
+    display: flex;
+    padding: 20px 0px;
+
+    &--top {
+      justify-content: space-between;
+
+      & > * {
+        width: 33.333%;
+        text-align: center;
+      }
+
+      @media screen and (min-width: $md) {
+        display: flex;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+      }
+    }
+
+    &--bottom {
+      display: none;
+      justify-content: center;
+
+      @media screen and (min-width: $md) {
+        display: flex;
+      }
+    }
+  }
+
+  &-btn-list {
+    display: flex;
+    column-gap: 20px;
+  }
+
+  &-left {
+    justify-content: end;
+
+    @media screen and (min-width: $md) {
+      justify-content: start;
+    }
   }
 
   &-right {
-    display: flex;
+    display: none;
+    justify-content: end;
+
+    @media screen and (min-width: $md) {
+      display: flex;
+    }
+  }
+
+  &-logo {
+    order: -1;
+    text-align: left;
+
+    @media screen and (min-width: $md) {
+      order: 0;
+      text-align: center;
+    }
+
+    &__link {
+      font-family: $font-clash-display;
+      font-size: 24px;
+    }
   }
 
   &-btn {
-    &:not(:first-child) {
-      margin-left: 20px;
+    &--toggle {
+      @media screen and (min-width: $md) {
+        display: none;
+      }
+    }
+  }
+
+  &-nav {
+    &__list {
+      display: flex;
+      column-gap: 45px;
     }
   }
 }
