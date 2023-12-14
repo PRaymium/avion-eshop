a8e054abd6c92e436cde7e96599f2c1daf61f76d
PRaymium <Jokersli@mail.ru>
2023-12-14 17:54:25 +0300

    created ProductPicture component

---
+++ src/components/ProductPicture.vue
@@ -0,0 +1,36 @@
+<template>
+  <picture class="product-picture">
+    <source
+      :media="`(max-width: ${breakpoints.xs}px)`"
+      :srcset="`${IMG_PATH}/product-${props.productId}-160w.jpg`"
+    />
+    <img
+      class="product-picture__img"
+      :src="`${IMG_PATH}/product-${props.productId}-300w.jpg`"
+      :alt="props.title"
+    />
+  </picture>
+</template>
+
+<script setup>
+import { breakpoints } from '../breakpoints'
+const IMG_PATH = 'img/products'
+
+const props = defineProps({
+  productId: {
+    type: Number,
+    required: true
+  },
+
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.product-picture {
+  width: 100%;
+}
+</style>
